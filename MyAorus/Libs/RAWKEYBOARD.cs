7ab1fabc686b59c9e538f32849a7a99787f2362a
nesh <kulplex@gmail.com>
2017-12-30 23:57:56 +0100

    general cleaning before next task

--- MyAorus/Libs/RAWKEYBOARD.cs
+++ MyAorus/Libs/RAWKEYBOARD.cs
@@ -1,10 +1,4 @@
-﻿// Decompiled with JetBrains decompiler
-// Type: AorusKeyboard.RAWKEYBOARD
-// Assembly: AorusFusion, Version=0.3.6.7, Culture=neutral, PublicKeyToken=null
-// MVID: D989AA71-FCE9-4831-9F62-8BDE4C7ACEEE
-// Assembly location: C:\Program Files (x86)\AorusFusion\AorusFusion.exe
-
-using System.Runtime.InteropServices;
+﻿using System.Runtime.InteropServices;
 
 namespace AorusKeyboard
 {
